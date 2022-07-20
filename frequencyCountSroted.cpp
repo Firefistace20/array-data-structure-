8447fd4eb7457d985dfb3a71b198ffca2c04ccbb
Firefistace20 <95499038+Firefistace20@users.noreply.github.com>
2022-07-20 12:24:10 +0530

    Add files via upload

---
+++ frequencyCountSroted.cpp
@@ -0,0 +1,59 @@
+
+//count the frequency of the element in array 
+#include <bits/stdc++.h>
+  using namespace std;
+
+ 
+// It prints number of
+// occurrences of each element in the array.
+  void
+findFrequency (int arr[], int n) 
+{
+  
+    // HashMap to store frequencies
+  map < int, int >mp;
+  
+ 
+    // traverse the array
+    for (int i = 0; i < n; i++)
+    {
+      
+	// update the frequency
+	mp[arr[i]]++;
+    
+} 
+ 
+    // traverse the hashmap
+for (auto i:mp)
+    {
+      
+cout << "Element " << i.first << " occurs " 
+	<<i.second << " times" << endl;
+    
+}
+
+}
+
+
+ 
+// Driver function
+  int
+main () 
+{
+  
+int arr[100], n;
+  
+cin >> n;
+  
+for (int i = 0; i < n; i++)
+    {
+      
+cin >> arr[i];
+    
+} 
+findFrequency (arr, n);
+  
+ 
+return 0;
+
+}
