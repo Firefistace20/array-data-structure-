1c2018d4ca80c4c1c6bd928cc7029da40dfa816d
Firefistace20 <95499038+Firefistace20@users.noreply.github.com>
2022-07-26 22:04:42 +0530

    Update klargestsmllest.cpp

--- klargestsmllest.cpp
+++ klargestsmllest.cpp
@@ -14,6 +14,11 @@ int main()
   }
   int k;
   cin>>k;
+  //declaration of the min heap if we want to find the largest k element in array we will use the min heap and vice versa 
+  //min element is present in the top of the min heap 
+//  priority_queue <int, vector<int>, greater<int> > pq;
+  
+  //max heap largest element is always in the top thats why we using this to find the smallest because we will pop the largest element every time if the size is greater then k 
   priority_queue<int >max;
   for(int i=0;i<n;i++){
       max.push(a[i]);
